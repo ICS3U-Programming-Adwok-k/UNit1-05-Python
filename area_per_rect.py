c2ba7345b447f4bce24bfb4175f9424936ae8f9f
Adwok Adiebo <adwok.adiebo@stu.ocsb.ca>
2025-02-24 17:44:36 +0000

    area_per-react.py

---
+++ area_per_rect.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Created by: Adwok Adiebo
+
+# Date: Feb 18, 2025
+
+# This program does area and perimeter of a rectangle with a length of 10cm and width of 5cm
+
+def main():
+    print("For the Area of a rectangle with length 10cm and width 5cm ")
+    print("The Area of the rectangle: {}cm^2". format(10 * 5))
+    print("The perimeter of the rectangle: {}cm". format((2*10) + (2*5)))
+
+if __name__ == "__main__":
+    main()
